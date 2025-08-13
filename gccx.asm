2f69f51a7a67b938107b5654448754db45b3c056
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-08-13 21:13:22 +0100

    Add files via upload

--- gccx.asm
+++ gccx.asm
@@ -1,20 +1,37 @@
-
+;nasm -f elf64 -o gccx.o gccx.asm
+;ld -dynamic-linker /lib64/ld-linux-x86-64.so.2    -o gccx /usr/lib/x86_64-linux-gnu/crt1.o    /usr/lib/x86_64-linux-gnu/crti.o    gccx.o    -lc    /usr/lib/x86_64-linux-gnu/crtn.o
+;gcc --version
+;./gccx
 [BITS 64]
-section .note.GNU-stack
-times 16384 db 0
+section .bss
+     resq 1024
 section .data
-    value128 dq 65, 0 ; 128-bit value
-    stg db 10,27,'[43;30m%d',10,10,10
+    times 16384 db 0
+    stacks:
+    times 16384 db 0
+
+    value128 dq 65, 0 ,0,0,0,0; 128-bit value
+    stg db 10,27,'[43;30m%ld',10,10,10,0
 section .text
     extern printf
     extern exit
     global main
 
 main:
+    mov rsp,stacks
+    sub rsp,64
+    movq xmm0,[value128]
+    movq rsi,xmm0
+    add rsp,64
     ; Encerrar (em Linux, syscall exit)
     mov rax,0
     mov rdi,stg
-    mov rsi,[value128]
+    
     call printf
-    call exit
+    ; Encerrar (em Linux, syscall exit)
+    mov eax, 60     ; syscall: exit
+    xor edi, edi    ; status 0
+    syscall
     ret
+section .note.GNU-stack
+    times 16384 db 0
