b58d1ca78d90f213a62e0300fc303180bee6dcc0
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-08-13 18:11:51 +0100

    Add files via upload

--- gccx.asm
+++ gccx.asm
@@ -1,29 +1,20 @@
 
 [BITS 64]
+section .note.GNU-stack
+times 16384 db 0
 section .data
     value128 dq 65, 0 ; 128-bit value
-
+    stg db 10,27,'[43;30m%d',10,10,10
 section .text
     extern printf
-    global _start
+    extern exit
+    global main
 
-_start:
-    ; Reservar espaço na pilha
-    sub rsp, 16
-
-    ; Carregar o valor de 128 bits para xmm1
-    movaps xmm1, [value128]
-
-    ; Fazer "push" do valor 128 bits (armazenar na pilha)
-    movaps [rsp], xmm1
-
-    ; Fazer "pop" do valor 128 bits (carregar de volta para xmm0)
-    movaps xmm0, [rsp]
-
-    ; Liberar espaço da pilha
-    add rsp, 16
-    movq rdi,xmm0
+main:
     ; Encerrar (em Linux, syscall exit)
-    mov eax, 60     ; syscall: exit
-    ;xor edi, edi    ; status 0
-    syscall
+    mov rax,0
+    mov rdi,stg
+    mov rsi,[value128]
+    call printf
+    call exit
+    ret
